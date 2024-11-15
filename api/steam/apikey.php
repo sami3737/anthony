8d79cbb08b1aacb00e559aea94c3cc09701929e3
sami3737 <46924423+sami3737@users.noreply.github.com>
2024-11-15 17:41:34 +0100

    Removing useless files

--- api/steam/apikey.php
+++ api/steam/apikey.php
@@ -1,5 +1,5 @@
 <?php
 
-	$api = "B6352E13B44254DD3F7BB98EDD0DC611";
+	$api = "";
 
 ?>
--- oldmenufourriere.php
+++
@@ -1,3 +0,0 @@
-
-<li><a href="./DPfourriere.php"><i class="fa fa-car"></i> Saisi fourriere</a></li>
-<li><a href="./DPsuivifourriere.php"><i class="fa fa-search"></i>Recherche Fourriere</a></li>
